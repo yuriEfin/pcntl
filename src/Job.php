8186e9bc6d30127c5c492596736f2a250ef216b9
Gambit <133@rpkenigma.ru>
2017-07-13 19:36:47 +0300

    ++

--- src/Job.php
+++ src/Job.php
@@ -152,6 +152,7 @@ class Job
             'is_running' => 0,
         ]);
         $this->model->update();
+        $this->context->unlockProcess($this->job->id);
 
         @unlink($this->context->getPathPid() . '/' . $this->pidFilename);
     }
